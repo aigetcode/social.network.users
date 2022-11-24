5d6a5fecf8b2cbb9640353b5aa7cdf12f3d52c9b
Ivanitsky Alexey <lexa3556@gmail.com>
2022-11-24 15:01:10 +0300

    MSA-3.4 Added docker compose

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
-## User service
+# User service
 This service handles basics operation with users and followers
 
 
-sorting="field, direction"
-
-
 ### Check test coverage (minimum 80%):
-`gradle build jacoco jacocoTestCoverageVerification`
+    `gradle build jacoco jacocoTestCoverageVerification`
 
 ### Build jacoco report:
-`gradle build jacoco jacocoTestReport`
+    `gradle build jacoco jacocoTestReport`
 
 Will building in `build/reports/test/html/index.html`
+
+
+## Docker compose
+
+#### 0. Build gradle project
+    ``gradle clean build``
+
+#### 1. Build service image
+
+    ``./servis.sh build``
+
+#### 2. Start containers
+
+    ``./servis.sh start``
+
+#### 2. Check that all containers are alive
+
+    ``docker ps -a``
+
+#### 3. Stop containers
+
+    ``./server.sh stop``
+
+#### 4. If you need to install from scratch - i.e. delete all data, then we perform a cleaning
+
+    ``./server.sh clean``
--- src/main/java/com/social/network/users/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/com/social/network/users/exceptions/ExceptionHandlerAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionHandlerAdvice {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ExceptionResponse<?>> handle(Throwable ex) {
+    public ResponseEntity<ExceptionResponse<BaseErrorResponse>> handle(Throwable ex) {
         log.error("Exception handling and serialization: " + ex.getMessage(), ex);
         BaseErrorResponse baseErrorResponse = new BaseErrorResponse(ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -21,7 +21,7 @@ public class ExceptionHandlerAdvice {
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ExceptionResponse<?>> handle(IllegalArgumentException ex) {
+    public ResponseEntity<ExceptionResponse<BaseErrorResponse>> handle(IllegalArgumentException ex) {
         log.error("Exception wrong arguments: " + ex.getMessage(), ex);
         BaseErrorResponse baseErrorResponse = new BaseErrorResponse(ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -30,7 +30,7 @@ public class ExceptionHandlerAdvice {
     }
 
     @ExceptionHandler({NotFoundException.class})
-    public ResponseEntity<ExceptionResponse<?>> handle(Exception ex) {
+    public ResponseEntity<ExceptionResponse<BaseErrorResponse>> handle(Exception ex) {
         log.error("Not found exception arguments: " + ex.getMessage(), ex);
         BaseErrorResponse baseErrorResponse = new BaseErrorResponse(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)

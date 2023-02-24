e5b5bc20c011d14df441d4e6c587e62aa9049fd4
Ivanitsky Alexey <to.developer.aleksey@gmail.com>
2023-02-24 17:55:52 +0300

    Added github image build for user repo

--- docker/users/build.sh
+++ docker/users/build.sh
@@ -4,12 +4,12 @@ set -e
 
 #script directory
 BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_VERSION=$(grep -oP 'version = "\K[^"\047]+(?=["\047])' $BUILD_DIR/../../../build.gradle.kts)
+PROJECT_VERSION=$(grep -oP 'version = "\K[^"\047]+(?=["\047])' $BUILD_DIR/../../build.gradle.kts)
 
 echo "Building image: social-users:$PROJECT_VERSION"
 
 
-cp "$BUILD_DIR/../../../build/libs/users-$PROJECT_VERSION.jar" "$BUILD_DIR/users.jar"
+cp "$BUILD_DIR/../../build/libs/users-$PROJECT_VERSION.jar" "$BUILD_DIR/users.jar"
 
 docker build -t "social-users:$PROJECT_VERSION" "$BUILD_DIR"
 

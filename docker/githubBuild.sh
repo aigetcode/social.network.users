2664ae8abb8a6c04086871e7278f4ea88993aefc
Ivanitsky Alexey <to.developer.aleksey@gmail.com>
2023-04-16 16:52:57 +0300

    Amazon-3.5 refactoring

--- docker/githubBuild.sh
+++ docker/githubBuild.sh
@@ -4,13 +4,13 @@ set -e
 
 #script directory
 BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_VERSION=$(grep -oP 'version = "\K[^"\047]+(?=["\047])' $BUILD_DIR/../../build.gradle.kts)
+PROJECT_VERSION=$(grep -oP 'version = "\K[^"\047]+(?=["\047])' $BUILD_DIR/../build.gradle.kts)
 
 echo "Building image: social-users:$PROJECT_VERSION"
 
 
 cp "$BUILD_DIR/../build/libs/users-$PROJECT_VERSION.jar" "$BUILD_DIR/users.jar"
-cd docker/users
+cd docker
 
 docker build . --tag "ghcr.io/aigetcode/social-users:$PROJECT_VERSION"
 echo "Built docker image"
